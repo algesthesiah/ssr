5fb27fdf509a3dcbfe151a51c158d12b8f704369
luleo <541877028@qq.com>
2022-04-14 18:53:20 +0800

    feat: update

---
+++ packages/types/rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve' // 帮助寻找 node_modules 里的包
+import postcss from 'rollup-plugin-postcss'
+import babel from '@rollup/plugin-babel'
+
+const config = [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        format: 'umd',
+        dir: 'lib',
+      },
+      {
+        format: 'es',
+        dir: 'es',
+      },
+    ],
+    external: [id => id.includes('@babel/runtime')],
+    plugins: [resolve(), commonjs(), postcss(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+  },
+]
+
+export default config
